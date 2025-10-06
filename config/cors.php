a56f3a8df14f43129430bd78fbdeaf3fdaf5e2f5
mantenimientoaguachica <mantecompuregionalaguachica@gmail.com>
2025-10-06 14:28:15 -0500

    CAMBIOS ANTES DE FORMATEAR

--- config/cors.php
+++ config/cors.php
@@ -2,24 +2,16 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173','http://192.168.1.93:5173', 'https://h.gpsmonitoreorada.site'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://192.168.1.93:5173',
+        'https://h.gpsmonitoreorada.site',
+        'https://p.gpsmonitoreorada.site',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +21,8 @@ return [
 
     'max_age' => 0,
 
+    // ⚠️ IMPORTANTE: si usas JWT con Authorization Header, déjalo en false.
+    // Si en algún momento quieres usar cookies con Sanctum → ponlo en true.
     'supports_credentials' => false,
 
 ];
